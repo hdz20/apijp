84e4cd49ee2f8a4e8a674c448d1500a7e7466071
hdz20 <134084736+hdz20@users.noreply.github.com>
2023-05-26 08:20:41 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,21 @@ app=FastAPI()
 
 @app.get("/")
 def index():
-	return "Hola preciosa, tas bimbonita"
+    return "hola a todos "
+@app.get("/tortas/{num}")
+def dav(num):
+    tortas={
+     1"jamon"
+      2"milanes"
+        3"pollo"
+        4"mole"
+    }
+    return tortas
+@app.get("/conversor_caf/{C}")
+def temp{C}:
+     try:
+            C=float(C)
+            TF=C*(9/5) + 32
+            return f"la temperatura es de {TF} grados sentigrados"
+    except:
+            return"eso no era"
