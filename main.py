1d0ab4bb6d4e9d36c90367390894bdbdc0b2e569
hdz20 <134084736+hdz20@users.noreply.github.com>
2023-05-26 10:03:30 -0600

    Update main.py

--- main.py
+++ main.py
@@ -9,18 +9,19 @@ def index():
 @app.get("/apijp/{num}")
 def carro(num):
     carros={
-     "1": "Audi",
-     "2": "BMW",
-     "3": "Mercedes Benz",
-     "4":" Porsche",
+    "1":"Audi",
+    "2":"BMW",
+    "3":"Mercedes Benz",
+    "4":"Porsche"
+        
     }
-    return (carros{num})
+    return carros{num}
 
 @app.get("/conversor_caf/{C}")
 def temp{C}:
      try:
             C=float(C)
             TF=C*(9/5) + 32
-            return f"la temperatura es de {TF} grados sentigrados"
+            return f"la temperatura es de {TF} grados centigrados"
     except:
             return"eso no era"
